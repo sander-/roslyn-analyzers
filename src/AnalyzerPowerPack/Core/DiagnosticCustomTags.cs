719c335286e03acde3ed40803ef33edd6e7a7c26
Sander Gerz <sander@devtips.net>
2015-11-05 12:01:58 -0800

    Document fix in DiagnosticCustomTags

--- src/AnalyzerPowerPack/Core/DiagnosticCustomTags.cs
+++ src/AnalyzerPowerPack/Core/DiagnosticCustomTags.cs
@@ -5,7 +5,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
     internal static class DiagnosticCustomTags
     {
         /// <summary>
-        /// it is string[] because DiagnosticDescriptor expects string[]. 
+        /// It is string[] because DiagnosticDescriptor expects string[]. 
         /// </summary>
         private static readonly string[] s_microsoftCustomTags = new string[] { WellKnownDiagnosticTags.Telemetry };
 
